bb02cb2bc103d5fd8899293473cc8b57227c2344
Epap <peia@inbox.ru>
2023-02-06 16:18:36 +0300

    Float point types

---
+++ Topics/Floating-point types/Area of a circle/src/Main.kt
@@ -0,0 +1,5 @@
+const val PI = 3.1415
+fun main() {
+    val r = readln().toInt()
+    println(r * r * PI)
+}
---
+++ Topics/Floating-point types/Liquid pressure/src/Main.kt
@@ -0,0 +1,7 @@
+const val G = 9.8
+fun main() {
+    val p = readln().toDouble()
+    val h = readln().toDouble()
+    println(p * G * h)
+
+}
